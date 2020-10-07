02cccdc16c4bcb13bfea95859172f835b1ae6632
AlbertoSavoldelli <62698244+AlbertoSavoldelli@users.noreply.github.com>
2020-10-07 09:49:45 +0200

    Aggiornato

---
+++ 03-Form con menù/03-Form con menù/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace _03_Form_con_menù
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnApriForm2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnApriForm2
+            // 
+            this.btnApriForm2.Location = new System.Drawing.Point(50, 32);
+            this.btnApriForm2.Name = "btnApriForm2";
+            this.btnApriForm2.Size = new System.Drawing.Size(277, 23);
+            this.btnApriForm2.TabIndex = 1;
+            this.btnApriForm2.Text = "APRI FORM 2 NO PARAMETRO";
+            this.btnApriForm2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnApriForm2);
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnApriForm2;
+    }
+}
+
---
+++ 03-Form con menù/03-Form con menù/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03_Form_con_menù
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
