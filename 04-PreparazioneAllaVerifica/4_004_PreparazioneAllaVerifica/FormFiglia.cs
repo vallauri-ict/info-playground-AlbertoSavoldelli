ab2be47eab85346c0e3c0344cbde5ba7e4848d94
Stud. Alberto Savoldelli <a.savoldelli.1375@vallauri.net>
2020-10-16 09:48:12 +0200

    Da terminare

---
+++ 04-PreparazioneAllaVerifica/4_004_PreparazioneAllaVerifica/FormFiglia.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_004_PreparazioneAllaVerifica
+{
+    public partial class FormFiglia : Form
+    {
+        public TextBox txt1;
+        public string value;
+
+        public FormFiglia()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnInviaStringa_Click(object sender, EventArgs e)
+        {
+            formMain.txtPassata.Text = txtStringaFiglia.Text;
+        }
+
+        private void FormFiglia_Load(object sender, EventArgs e)
+        {
+             txt1= txtStringaFiglia;
+        }
+
+
+        //public void cambiaTesto()
+        //{
+        //    formMain.txtPassata = txt1;
+        //}
+    }
+}
