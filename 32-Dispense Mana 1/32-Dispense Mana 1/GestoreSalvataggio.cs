1ee8a47df560f083387be479fc4b523da1fcd0f3
AlbertoSavoldelli <a.savoldelli.1375@vallauri.edu>
2021-03-04 22:02:29 +0100

    terminated and working

---
+++ 32-Dispense Mana 1/32-Dispense Mana 1/GestoreSalvataggio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _32_Dispense_Mana_1
+{
+    public delegate void salvataggioEseguitoEventHandler(int id);
+    class GestoreSalvataggio
+    {
+        public event salvataggioEseguitoEventHandler salvataggioEseguito;
+
+        public void salvaSuFile(int id)
+        {
+
+            MessageBox.Show("Sto eseguendo il salvataggio");
+
+            salvataggioEseguito(id);
+        }
+    }
+}
